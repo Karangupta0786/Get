5d0e43e998252d77ac35254dadf1d8f8555086fb
Karan <karansuggu@gmail.com>
2023-09-24 19:08:20 +0530

    code clean up.

--- app/src/main/java/com/example/getapikt/ApiInterface.kt
+++ app/src/main/java/com/example/getapikt/ApiInterface.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface ApiInterface {
 
-    @GET("posts")
+    @GET("posts") // here is end URL in parameter of GET
 
     fun getData(): Call<myData>
 }
--- app/src/main/java/com/example/getapikt/MainActivity.kt
+++ app/src/main/java/com/example/getapikt/MainActivity.kt
@@ -1,37 +1,28 @@
 package com.example.getapikt
 
-import android.content.Intent
-import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
-import com.google.android.material.snackbar.Snackbar
-import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+
 
 const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn_next:Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn_next.setOnClickListener {
-            startActivity(Intent(this@MainActivity,MainActivity2::class.java))
-        }
 
             getMyData()
     }
@@ -63,11 +54,6 @@ class MainActivity : AppCompatActivity() {
                 else{
                     Toast.makeText(applicationContext, "Mission Failed", Toast.LENGTH_SHORT).show()
                 }
-
-//                val sharedPreferences:SharedPreferences = getSharedPreferences("sharedPref",
-//                    MODE_PRIVATE)
-//                val editor:Editor = sharedPreferences.edit()
-
 
             }
 
